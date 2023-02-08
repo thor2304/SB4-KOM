aa41a23c9c0107788196274aa3cdab305e106110
Thor Jørgin <thorsekstra@gmail.com>
2023-02-08 09:31:02 +0100

    Added support for newer Java jdks by upgrading the backend to lwjgl3

--- AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ AsteroidsLibGDX/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -1,21 +1,22 @@
 package dk.sdu.mmmi.cbse.main;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class Main {
 	
 	public static void main(String[] args) {
+
+		Lwjgl3ApplicationConfiguration cfg =
+			new Lwjgl3ApplicationConfiguration();
+		cfg.setTitle("Asteroids");
+		cfg.setResizable(false);
+//		cfg.width = 500;
+//		cfg.height = 400;
+//		cfg.useGL30 = false;
+//		cfg.resizable = false;
 		
-		LwjglApplicationConfiguration cfg =
-			new LwjglApplicationConfiguration();
-		cfg.title = "Asteroids";
-		cfg.width = 500;
-		cfg.height = 400;
-		cfg.useGL30 = false;
-		cfg.resizable = false;
-		
-		new LwjglApplication(new Game(), cfg);
+		new Lwjgl3Application(new Game(), cfg);
 		
 	}
 	
