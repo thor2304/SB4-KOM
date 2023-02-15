96ebf83055f6c57ee4c7622c95da19d3b73898e2
Thor Jørgin <tjoer21@student.sdu.dk>
2023-02-15 10:08:55 +0100

    Interfaces documented

--- AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
+++ AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
@@ -6,7 +6,9 @@ import dk.sdu.mmmi.cbse.common.data.World;
 public interface IEntityProcessingService {
 
     /**<h2>Description:</h2>
-     * Called to update a specific set of components, that exist in the world. <br>
+     * Called to update movement and similar behaviour of a specific set of components, that exist in the world.
+     * This process can and should be used for behaviour that can or must happen before collision checks.
+     * <br>
      * <br>
      * <h2>Pre:</h2>
      * The previous call to process must have finished.
--- AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
+++ AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
@@ -8,5 +8,17 @@ import dk.sdu.mmmi.cbse.common.data.World;
  * @author jcs
  */
 public interface IPostEntityProcessingService  {
+        /**<h2>Description:</h2>
+         * Called after the movement updates have happened from {@link IEntityProcessingService#process(GameData, World)}.
+         * <br><br>
+         * <h2>Pre:</h2>
+         * The entities must be started, and before each call to this process, {@link IEntityProcessingService#process(GameData, World)} must be called.
+         * <br><br>
+         * <h2>Post:</h2>
+         * The entities in the world will have been post-processed.
+         * <br><br>
+         * @param gameData Information about the Game, such as screen size etc.
+         * @param world The world object that the Plugin will be added to.
+         */
         void process(GameData gameData, World world);
 }
